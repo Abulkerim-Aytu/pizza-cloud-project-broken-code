9f5069250be9d407d5391d85362c6bd6bd6093bf
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-24 13:47:13 +0300

    fixed - 9

--- src/main/java/com/cydeo/controller/OrderController.java
+++ src/main/java/com/cydeo/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.exception.PizzaNotFoundException;
 import com.cydeo.model.Pizza;
 import com.cydeo.model.PizzaOrder;
 import com.cydeo.repository.PizzaRepository;
@@ -33,7 +34,7 @@ public class OrderController {
     }
 
     @PostMapping("/{pizzaId}")
-    public String processOrder(UUID pizzaId, PizzaOrder pizzaOrder) {
+    public String processOrder(@PathVariable UUID pizzaId, PizzaOrder pizzaOrder) {
 
         // Save the order
 
@@ -42,9 +43,10 @@ public class OrderController {
     }
 
     //TODO
-    private Pizza getPizza(UUID pizzaId) {
+    private Pizza getPizza(UUID pizzaId) throws PizzaNotFoundException {
         // Get the pizza from repository based on it's id
-        return new Pizza();
+        return pizzaRepository.readAll().stream().filter(p -> p.getId().equals(pizzaId))
+                .findFirst().orElseThrow(() -> new PizzaNotFoundException("Pizza not found"));
     }
 
 }
